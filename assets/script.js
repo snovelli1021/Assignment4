d3ec7c44489e9693e59a5411931ba5a0b1582ecc
Stephen Novelli <snovelli1021@gmail.com>
2022-07-30 15:40:33 -0700

    Fixed start quiz button and adding the question box area, and timer function.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,24 @@
-const startBtn = document.querySelector(".startBtn");
+const startBtn = document.querySelector("#startBtn");
+const quiz_box = document.querySelector(".quiz_box");
+let timerEl = document.getElementById('timer');
 
-
-
+//Coundown timer. Does not reduce time if wrong choice yet.
+function countdown() {
+    var timeLeft = 60;
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
+  
+  countdown();
 
